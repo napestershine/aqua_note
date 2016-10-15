5a1b6ade9b177b7c144eb0a6d437564d920f37a4
root <napestershine@gmail.com>
2016-10-15 17:37:13 +0530

    created genus controller and show action

---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class GenusController extends Controller
+{
+    /**
+     * @Route("/genus")
+     */
+    public function showAction()
+    {
+        return new Response("Under the see");
+
+    }
+}
