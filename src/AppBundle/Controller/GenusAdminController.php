a17c44426247b9b3b4e6abb99327f1242be72b23
root <napestershine@gmail.com>
2016-10-24 02:12:15 +0530

    added form themeing

--- src/AppBundle/Controller/GenusAdminController.php
+++ src/AppBundle/Controller/GenusAdminController.php
@@ -2,12 +2,37 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\GenusFormType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * @Route("/admin")
+ */
 class GenusAdminController extends Controller
 {
-    public function indexAction($name)
+    /**
+     * @Route("/genus", name="admin_genus_list")
+     */
+    public function indexAction()
     {
-        return $this->render('', array('name' => $name));
+        $genuses = $this->getDoctrine()
+            ->getRepository('AppBundle:Genus')
+            ->findAll();
+        return $this->render('admin/genus/list.html.twig', array(
+            'genuses' => $genuses
+        ));
     }
+
+    /**
+     * @Route("/genus/new", name="admin_genus_new")
+     */
+    public function newAction()
+    {
+        $form = $this->createForm(GenusFormType::class);
+        return $this->render('admin/genus/new.html.twig', [
+            'genusForm' => $form->createView()
+        ]);
+    }
+
 }
