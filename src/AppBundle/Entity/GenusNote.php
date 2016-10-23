3563be83d1549a5f5b0cfdb7b290b831d0293e21
root <napestershine@gmail.com>
2016-10-23 06:42:44 +0530

    created new genus note class

---
+++ src/AppBundle/Entity/GenusNote.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 23/10/16
+ * Time: 6:40 AM
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="genus_note")
+ */
+class GenusNote
+{
+
+}
