e9ef11b6e0382d928cb93eeb30c4c024e8f1f0c5
root <napestershine@gmail.com>
2016-10-23 06:57:18 +0530

    added many to one relationship

--- src/AppBundle/Entity/GenusNote.php
+++ src/AppBundle/Entity/GenusNote.php
@@ -44,6 +44,11 @@ class GenusNote
     private $createdAt;
 
     /**
+     * @ORM\ManyToOne(targetEntity="Genus")
+     */
+    private $genus;
+
+    /**
      * @return mixed
      */
     public function getId()
@@ -121,6 +126,22 @@ class GenusNote
     public function setCreatedAt($createdAt)
     {
         $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGenus()
+    {
+        return $this->genus;
+    }
+
+    /**
+     * @param mixed $genus
+     */
+    public function setGenus(Genus $genus)
+    {
+        $this->genus = $genus;
     }
 
 }
