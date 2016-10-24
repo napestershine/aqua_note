23dc51eef1f45a667e887b47312f0e9a82190477
root <napestershine@gmail.com>
2016-10-24 17:41:49 +0530

    created User Entity

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 24/10/16
+ * Time: 5:39 PM
+ */
+
+namespace AppBundle\Entity;
+
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class User implements UserInterface
+{
+    public function getUsername()
+    {
+        // TODO: Implement getUsername() method.
+    }
+
+    public function getRoles()
+    {
+        // TODO: Implement getRoles() method.
+    }
+
+    public function getPassword()
+    {
+        // TODO: Implement getPassword() method.
+    }
+
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+}
